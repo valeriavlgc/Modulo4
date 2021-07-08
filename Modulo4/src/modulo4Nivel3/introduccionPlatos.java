5d965d75f0538b3d78ea38ab3d6ef1834bfa4359
Valeria Lavin <valeriavlgc@gmail.com>
2021-07-08 09:30:46 +0200

    Módulo 4

--- Modulo4/src/modulo4Nivel3/introduccionPlatos.java
+++
@@ -1,8 +0,0 @@
-package modulo4Nivel3;
-
-public class introduccionPlatos extends RuntimeException {
-	public introduccionPlatos() {}
-	public introduccionPlatos(String mensaje) {
-		super(mensaje);
-    }
-}
--- Modulo4/src/modulo4Nivel3/revisionPlatos.java
+++
@@ -1,8 +0,0 @@
-package modulo4Nivel3;
-
-public class revisionPlatos extends RuntimeException {
-	public revisionPlatos() {}
-	public revisionPlatos(String mensaje) {
-		super(mensaje);
-    }
-}
